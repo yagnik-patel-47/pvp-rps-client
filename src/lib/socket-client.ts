624230160483f5cd8ab5c40a279776b420793bd1
Yagnik Patel <coderisaddicted@gmail.com>
2025-02-06 01:51:42 +0530

    env var

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -1,1 +1,6 @@
-export const ws = new WebSocket("ws://localhost:3000");
+import { dev } from "$app/environment";
+import { PUBLIC_SERVER_ENDPOINT } from "$env/static/public";
+
+export const ws = new WebSocket(
+	`ws://${dev ? "localhost:3000" : PUBLIC_SERVER_ENDPOINT}`,
+);
