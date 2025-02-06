b0ece6ddeeac110c2fa14d62e63554c1fb1a401f
Yagnik Patel <coderisaddicted@gmail.com>
2025-02-06 15:02:18 +0530

    correct connection

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -2,5 +2,5 @@ import { dev } from "$app/environment";
 import { PUBLIC_SERVER_ENDPOINT } from "$env/static/public";
 
 export const ws = new WebSocket(
-	`ws://${dev ? "localhost:3000" : PUBLIC_SERVER_ENDPOINT}`,
+	`${dev ? "ws" : "wss"}://${dev ? "localhost:3000" : PUBLIC_SERVER_ENDPOINT}`,
 );
