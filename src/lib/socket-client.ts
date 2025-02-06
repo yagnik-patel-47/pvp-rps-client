3a16d518f959b1a3f2d1a6c87992b939e8f610be
Yagnik Patel <coderisaddicted@gmail.com>
2025-02-06 15:06:26 +0530

    fix: ws undefined on server

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -1,6 +1,9 @@
 import { dev } from "$app/environment";
 import { PUBLIC_SERVER_ENDPOINT } from "$env/static/public";
+import { browser } from "$app/environment";
 
-export const ws = new WebSocket(
-	`${dev ? "ws" : "wss"}://${dev ? "localhost:3000" : PUBLIC_SERVER_ENDPOINT}`,
-);
+export const ws = browser
+	? new WebSocket(
+			`${dev ? "ws" : "wss"}://${dev ? "localhost:3000" : PUBLIC_SERVER_ENDPOINT}`,
+		)
+	: null;
